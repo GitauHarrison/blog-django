743566471080b0cc0c3e197d9b627cf4525179ad
harry <harrisongitau3@gmail.com>
2022-11-27 11:01:26 +0300

    Build: Work with templates

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,28 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+
+posts = [
+    {
+        'author': 'Harry',
+        'title': 'Post 1',
+        'content': 'First post content',
+        'date_posted': 'Dec 27, 2022'
+    },
+    {
+        'author': 'Muthoni',
+        'title': 'Post 2',
+        'content': 'Second post content',
+        'date_posted': 'Dec 26, 2022'
+    }
+]
+
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
