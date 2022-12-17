25e48247828304e04eca1b6d72a1c187bbd7dc56
harry <harrisongitau3@gmail.com>
2022-12-17 11:09:28 +0300

    Build: Add migrations and update the database

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    timestamp = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __repr__(self):
+        return f'Post: {self.title}'
--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,10 @@
 from django.shortcuts import render
-
-
-posts = [
-    {
-        'author': 'Harry',
-        'title': 'Post 1',
-        'content': 'First post content',
-        'date_posted': 'Dec 27, 2022'
-    },
-    {
-        'author': 'Muthoni',
-        'title': 'Post 2',
-        'content': 'Second post content',
-        'date_posted': 'Dec 26, 2022'
-    }
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
