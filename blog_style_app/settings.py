f7529d2f7fc88b80fb157a33b447ae5c4b26c4cc
harry <harrisongitau3@gmail.com>
2022-12-18 04:21:38 +0300

    Build(feat): Add a user authentication system
    - Add a profile page
    - Modify the redirect from django defaults to custom ones

--- blog_style_app/settings.py
+++ blog_style_app/settings.py
@@ -121,6 +121,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# Crispy forms
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
@@ -128,3 +129,7 @@ CRISPY_TEMPLATE_PACK = 'bootstrap5'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Updating default URLs
+LOGIN_REDIRECT_URL = 'blog-home'
+LOGIN_URL = 'login'
